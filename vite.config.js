5d279e643ff2af11aac513058235e2013f14de1b
Orgha Tomal Roy <orgha.tomal@welldev.io>
2025-02-17 11:01:58 +0600

    feature: added manifest items for pwa

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,49 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Rhätische Bahn",
+        short_name: "RhB",
+        description: "Application for RhB Train Tacking",
+        theme_color: "#EB0A23",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      start_url: "/",
+      display: "standalone",
+      background_color: "#EB0A23",
+      theme_color: "#EB0A23",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
